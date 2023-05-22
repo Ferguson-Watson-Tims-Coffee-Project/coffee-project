cc6fa6c6725d26852386fa8cc725fad6b23b8c62
Brenden <brenden.m.ferguson@gmail.com>
2023-05-22 12:04:25 -0500

    Added finishing touches along with some comments

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,9 @@
 "use strict";
 
+(function (){
+
+//Gives each Coffee their own div and lists the roast along the side of it
+
 function renderCoffee(coffee) {
     var html = '<div class="coffee d-flex justify-content-between col-5 border rounded-3 p-1 bg-transparent my-2" style="color: white">';
     html += '<p class="fw-bold me-2 mb-0" style="font-size:2vw">' + coffee.name + '</p>';
@@ -9,6 +13,8 @@ function renderCoffee(coffee) {
     return html;
 }
 
+//renders the coffees onto the page
+
 function renderCoffees(coffees) {
     var html = '';
     for (var i = 0; i < coffees.length; i++) {
@@ -16,6 +22,8 @@ function renderCoffees(coffees) {
     }
     return html;
 }
+
+// updates which coffees should be shown
 
 function updateCoffees(e) {
     e.preventDefault();
@@ -28,8 +36,19 @@ function updateCoffees(e) {
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
+    var tbody = document.querySelector('#coffees');
 
-//takes in the user input and only displays coffee name that includes the input
+//updates the displayed Coffee(s) depending on the roast and search box input
+
+    var roastSelection = document.querySelector('#roast-selection');
+    roastSelection.addEventListener('change', updateCoffees);
+    roastSelection.addEventListener('change', searchCoffees);
+
+    let searchRoast = document.querySelector('#coffee-search');
+    searchRoast.addEventListener('input', searchCoffees);
+
+//takes in the user input and only displays coffee name that includes the input under selected roast
+
 function searchCoffees(e) {
     e.preventDefault();
     var selectedRoast = roastSelection.value;
@@ -43,12 +62,23 @@ function searchCoffees(e) {
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
 
+//adds a new coffee to the Coffees array
+
 function addCoffee() {
     let coffeeId = coffees.length + 1
     let userCoffee = {id: coffeeId, name: userCoffeeName.value, roast: userRoastSelection.value}
     coffees.push(userCoffee);
     tbody.innerHTML = renderCoffees(coffees);
 }
+
+    let userCoffeeName = document.querySelector('#user-coffee-name');
+    let userRoastSelection = document.querySelector('#user-roast-type');
+
+// Code to Add Coffee to Coffee List
+
+let userCoffeeSubmit = document.querySelector('#add-coffee');
+userCoffeeSubmit.addEventListener('click', addCoffee);
+userCoffeeSubmit.addEventListener('click', searchCoffees);
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
@@ -68,23 +98,12 @@ var coffees = [
     {id: 14, name: 'French', roast: 'dark'},
 ];
 
-console.log(coffees);
-
-var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
-var roastSelection = document.querySelector('#roast-selection');
-let searchRoast = document.querySelector('#coffee-search');
-let userCoffeeName = document.querySelector('#user-coffee-name');
-let userRoastSelection = document.querySelector('#user-roast-type');
-let userSubmit = document.querySelector('#add-coffee');
+submitButton.addEventListener('click', updateCoffees);
 
 tbody.innerHTML = renderCoffees(coffees);
 
-submitButton.addEventListener('click', updateCoffees);
-searchRoast.addEventListener('input', searchCoffees);
-roastSelection.addEventListener('change', updateCoffees);
-roastSelection.addEventListener('change', searchCoffees);
-userSubmit.addEventListener('click', addCoffee);
-userSubmit.addEventListener('click', searchCoffees);
 
 
+
+})();
